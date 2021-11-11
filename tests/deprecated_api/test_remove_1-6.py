c8a10b3b5c21430f771007abb513e7aae9c0ce4f
rohitgr7 <rohitgr1998@gmail.com>
2021-11-11 17:00:05 +0530

    remove deprecated reload_dataloaders_every_epoch

--- tests/deprecated_api/test_remove_1-6.py
+++ tests/deprecated_api/test_remove_1-6.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Test deprecated functionality which will be removed in v1.6.0."""
-from unittest.mock import call, Mock
-
 import pytest
 
 from pytorch_lightning import Trainer
@@ -30,35 +28,6 @@ def test_old_transfer_batch_to_device_hook(tmpdir):
     trainer = Trainer(default_root_dir=tmpdir, limit_train_batches=1, limit_val_batches=0, max_epochs=1)
     with pytest.deprecated_call(match="old signature will be removed in v1.6"):
         trainer.fit(OldModel())
-
-
-def test_v1_6_0_reload_dataloaders_every_epoch(tmpdir):
-    model = BoringModel()
-
-    tracker = Mock()
-    model.train_dataloader = Mock(wraps=model.train_dataloader)
-    model.val_dataloader = Mock(wraps=model.val_dataloader)
-    model.test_dataloader = Mock(wraps=model.test_dataloader)
-
-    tracker.attach_mock(model.train_dataloader, "train_dataloader")
-    tracker.attach_mock(model.val_dataloader, "val_dataloader")
-    tracker.attach_mock(model.test_dataloader, "test_dataloader")
-
-    with pytest.deprecated_call(match="`reload_dataloaders_every_epoch` is deprecated in v1.4 and will be removed"):
-        trainer = Trainer(
-            default_root_dir=tmpdir,
-            limit_train_batches=0.3,
-            limit_val_batches=0.3,
-            reload_dataloaders_every_epoch=True,
-            max_epochs=3,
-        )
-    trainer.fit(model)
-    trainer.test()
-
-    expected_sequence = (
-        [call.val_dataloader()] + [call.train_dataloader(), call.val_dataloader()] * 3 + [call.test_dataloader()]
-    )
-    assert tracker.mock_calls == expected_sequence
 
 
 def test_v1_6_0_is_overridden_model():
